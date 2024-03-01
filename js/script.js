dacfe15c5d45ef5714a5557f7483bcb2fa6ec236
Sovanthorn <nounsovannthon@gmail.com>
2024-03-02 00:58:27 +0700

    correct my error

--- js/script.js
+++ js/script.js
@@ -1,19 +1,18 @@
-'use strict'
-
-import{ cardProduct } from "../component/cardProduct.js"
+"use strict";
+import { cardProduct } from "../component/cardProduct.js";
 import { getData } from "../store/fectchApi.js";
 import { userCard } from "../component/userCard.js";
 
 const renderArea = document.querySelector("#render-area");
-const cardProduct = await getData("products");
+const cardProducts = await getData("products");
 
-const userCards = document.querySelector("#Users");
-const cardProduct2 = await getData("users");
+const userLists = document.querySelector("#userList");
+const users = await getData("users");
 
-cardProduct.map((product) => {
-    renderArea.innerHTML += cardProduct(product);
-  });
-  
-  cardProduct2.map((user) => {
-    userCards.innerHTML += userCard(user);
-  });
+cardProducts.map((product) => {
+  renderArea.innerHTML += cardProduct(product);
+});
+
+users.map((user) => {
+  userLists.innerHTML += userCard(user);
+});
